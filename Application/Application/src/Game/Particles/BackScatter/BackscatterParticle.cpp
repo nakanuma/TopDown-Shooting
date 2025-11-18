a3488f6ef3f30da91fed05cfee273663719dbb1b
nakanuma <k023g0121@g.neec.ac.jp>
2025-11-18 16:56:47 +0900

    マジックナンバーの解消（Particles : Backscatter, BloodSmoke, BloodSplatter）

--- Application/Application/src/Game/Particles/BackScatter/BackscatterParticle.cpp
+++ Application/Application/src/Game/Particles/BackScatter/BackscatterParticle.cpp
@@ -23,21 +23,20 @@ BackscatterParticleData BackscatterParticle::CreateParticle(const Float3& pos, c
 	// 位置
 	p.transform.translate_ = pos;
 	// スケール
-	p.transform.scale_ = {0.06f, 0.06f, 0.6f};
+	p.transform.scale_ = kScale;
 	// 速度ベクトル
-	Float3 baseDir = Float3::Normalize(velocity) * -1.0f; // 引数で受け取った方向と逆向きにする
-	float diff = 0.4f;
-	Float3 randDir = rand->RandomValue({-diff, 0.0f, -diff}, {diff, 0.0f, diff}); // 方向をバラつかせるためのオフセット
-	p.velocity = Float3::Normalize(baseDir + randDir) * rand->RandomValue(12.0f, 24.0f);
+	Float3 baseDir = Float3::Normalize(velocity) * -1.0f;                                                                         // 引数で受け取った方向と逆向きにする
+	Float3 randDir = rand->RandomValue({-kDirectionSpread, 0.0f, -kDirectionSpread}, {kDirectionSpread, 0.0f, kDirectionSpread}); // 方向をバラつかせるためのオフセット
+	p.velocity = Float3::Normalize(baseDir + randDir) * rand->RandomValue(kMinSpeed, kMaxSpeed);
 	// 回転（進行方向を向くように）
 	Float3 dir = Float3::Normalize(p.velocity);
 	float yaw = std::atan2(dir.x, dir.z);
 	float pitch = -std::asin(dir.y);
 	p.transform.rotate_ = {-pitch, -yaw, 0.0f};
 	// 色
-	p.color = {1.0f, 1.0f, 1.0f, 1.0f};
+	p.color = kInitialColor;
 	// 生存時間
-	p.lifeTime = rand->RandomValue(0.3f, 0.5f);
+	p.lifeTime = rand->RandomValue(kMinLifeTime, kMaxLifeTime);
 	// 経過時間
 	p.currentTime = 0.0f;
 	// 初期スケール
@@ -59,18 +58,18 @@ void BackscatterParticle::UpdateParticle(BackscatterParticleData& p, float dt) {
 
 	// 色
 	Float4 color;
-	if (t < 0.5f) {
+	if (t < kColorTransitionPoint) {
 		// 前半 : 白->橙
-		float localT = t / 0.5f; // 0~1に正規化
-		color.x = Easing::Lerp(1.0f, 1.0f, localT);
-		color.y = Easing::Lerp(1.0f, 0.5f, localT);
-		color.z = Easing::Lerp(1.0f, 0.0f, localT);
+		float localT = t / kColorTransitionPoint; // 0~1に正規化
+		color.x = Easing::Lerp(kColorWhite.x, kColorOrange.x, localT);
+		color.y = Easing::Lerp(kColorWhite.y, kColorOrange.y, localT);
+		color.z = Easing::Lerp(kColorWhite.z, kColorOrange.z, localT);
 	} else {
 		// 後半 : 橙->赤
-		float localT = (t - 0.5f) / 0.5f; // 0~1に正規化
-		color.x = Easing::Lerp(1.0f, 1.0f, localT);
-		color.y = Easing::Lerp(0.5f, 0.0f, localT);
-		color.z = Easing::Lerp(0.0f, 0.0f, localT);
+		float localT = (t - kColorTransitionPoint) / (1.0f - kColorTransitionPoint); // 0~1に正規化
+		color.x = Easing::Lerp(kColorOrange.x, kColorRed.x, localT);
+		color.y = Easing::Lerp(kColorOrange.y, kColorRed.y, localT);
+		color.z = Easing::Lerp(kColorOrange.z, kColorRed.z, localT);
 	}
 	color.w = 1.0f;
 
