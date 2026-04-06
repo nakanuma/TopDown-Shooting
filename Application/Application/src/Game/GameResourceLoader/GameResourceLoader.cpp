11bc7719c6ec8615591b101578a659e75ba28afd
nakanuma <k023g0121@g.neec.ac.jp>
2026-04-06 16:13:45 +0900

    カーソル非表示

--- Application/Application/src/Game/GameResourceLoader/GameResourceLoader.cpp
+++ Application/Application/src/Game/GameResourceLoader/GameResourceLoader.cpp
@@ -45,8 +45,7 @@ void GameResourceLoader::Initialize() {
 	Cygnus::SkyBoxManager::GetInstance()->Initialize("skybox.dds");
 
 	// カーソル非表示 + フルスクリーン化
-	/*ShowCursor(FALSE);*/
-	/*Cygnus::Window::ToggleFullscreen();*/
+	ShowCursor(FALSE);
 }
 
 void GameResourceLoader::LoadAllModelData() {
